53f446f0fae410b57eb50308c5a9b40930e11175
Cory Sebastian <sebastiancory@yahoo.com>
2020-06-21 13:49:20 -0500

    minor tweaks to the greeting class

--- src/Greeting.java
+++ src/Greeting.java
@@ -1,25 +1,27 @@
 import java.util.Scanner;
+
 public class Greeting {
+
   public static Scanner lou = new Scanner(System.in);
 
   public static boolean userGreeting() {
-    String commence;
     System.out.println("Hello! welcome to G Lous Stupid Game!");
     System.out.println("If you don't know the rules....");
     System.out.println(Ansi.RED + "\nGOOGLE " + Ansi.RESET + "them. Just search \"3-13 card game!\"");
     System.out.println("If you are ready to begin enter \"yes\" or enter \"no\"!");
+
+    String commence;
     lou = new Scanner(System.in);
-    while (lou.hasNextLine()) {
+
+    while (lou.hasNextLine())
+    {
       commence = lou.nextLine();
-      if (commence.equalsIgnoreCase("yes")) {
+      if (commence.equalsIgnoreCase("yes") || commence.equalsIgnoreCase("y") )
         return true;
-      }
-      else if (commence.equalsIgnoreCase("no")) {
+      else if (commence.equalsIgnoreCase("no"))
         return false;
-      }
-      else {
+      else
         System.out.println("Enter \"yes\" to play and \"no\" to quit");
-      }
     }
     return false;
   } // end user userGreeting
