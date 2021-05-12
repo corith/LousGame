b2e03bb18f07815efea52dbfe10ded034d76f663
Cory Sebastian <sebastiancory@yahoo.com>
2021-05-12 11:56:19 -0400

    Refactor for DeckOfCards added java docs

--- src/Greeting.java
+++ src/Greeting.java
@@ -7,7 +7,12 @@ public class Greeting {
 
     public static Scanner lou = new Scanner(System.in);
 
-    public static boolean userGreeting() {
+    /**
+     * Prompts user with a welcome message and asks if they are ready to play the game.
+     * This is called by handleRealUser() below.
+     * @return true if answer is yes and false if answer is no.
+     */
+    private static boolean userGreeting() {
         System.out.println("Hello! welcome to G Lous Stupid Game!");
         System.out.println("If you don't know the rules....");
         System.out.println(Ansi.RED + "\nGOOGLE " + Ansi.RESET + "them. Just search \"3-13 card game!\"");
@@ -28,6 +33,10 @@ public class Greeting {
         return false;
     }
 
+    /**
+     * This is called at the beginning of LousReady and calls
+     * @return true if user want's to play and false if they do not
+     */
     public static boolean handleRealUser() {
         if (!Greeting.userGreeting()) {
             System.out.println("Okay.....goodbye.");
--- src/Player.java
+++ src/Player.java
@@ -212,7 +212,6 @@ public class Player
 
 class PlayerTest
 {
-/**************** BEIGN MAIN METHOD FOR TESTS ***********/
   public static void main(String[] args)
   {
     Player one = new Player();
